65dc3715ab18c680191f61e3c6b7d081d6a0993f
KubilayC <kubilaycansever@hotmail.com>
2024-08-28 04:01:04 +0300

    update

--- 2_spotifyDataAnalysis/codes/spotifyQuery.sql
+++
@@ -1,7 +0,0 @@
-
--- tiktok mu spoti mi daha fazla
-/*SELECT 
-    COUNT(CASE WHEN spotify_streams > tiktok_views THEN 1 END) AS 'spotify>tiktok',
-    COUNT(CASE WHEN spotify_streams < tiktok_views THEN 1 END) AS 'tiktok>spotify'
-FROM 
-    spotify;*/
